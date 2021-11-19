33f9eb3ce6a746ac7e6d20032171e799b5ebb3f6
veenaguruprasad <veena@bmsit.in>
2021-11-19 19:36:39 +0530

    Checkbox and strike-through for completed todos added

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -11,7 +11,7 @@ class Todo < ActiveRecord::Base
   end
 
   def self.overdue
-    where("due_date < ?", Date.today)
+    where("due_date < ? and completed = ?", Date.today, false)
   end
 
   def self.due_today
